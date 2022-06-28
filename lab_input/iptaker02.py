1e1fa503e46bf7f79b9645f7fb8d817a1ed0abd7
Joey H <joeyhjava@users.noreply.github.com>
2022-06-28 19:43:54 +0000

    join and split

--- lab_input/iptaker02.py
+++ lab_input/iptaker02.py
@@ -5,13 +5,14 @@
 def main():
 
     # collect string input from the user
-    user_input = input("Please enter an IPv4 IP address:")
+    name = input("What's your name? ")
+    day = input("What day of week is it? ")
     
     ## the line below creates a single string that is passed to print()
     # print("You told me the IPv4 address is: " + user_input)
     
     ## print() can be given a series of objects separated by a comma
-    print("You told me the IPv4 address is:", user_input)
+    print(f'Hello {name}! Happy {day}!')
 
 main()
 
